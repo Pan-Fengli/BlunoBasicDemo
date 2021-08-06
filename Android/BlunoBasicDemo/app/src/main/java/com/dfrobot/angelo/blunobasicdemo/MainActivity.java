d50f6dd60f4dc81a96fdb085b9e6d3b1f63f40a0
PanFengli <1159929061@qq.com>
2021-08-06 14:51:22 +0800

    fix：修改源码，可以跑起来

--- Android/BlunoBasicDemo/app/src/main/java/com/dfrobot/angelo/blunobasicdemo/MainActivity.java
+++ Android/BlunoBasicDemo/app/src/main/java/com/dfrobot/angelo/blunobasicdemo/MainActivity.java
@@ -1,6 +1,10 @@
 package com.dfrobot.angelo.blunobasicdemo;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
@@ -15,12 +19,15 @@ public class MainActivity  extends BlunoLibrary {
 	private Button buttonSerialSend;
 	private EditText serialSendText;
 	private TextView serialReceivedText;
-	
+	public static final int ACCESS_LOCATION = 1;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-        onCreateProcess();														//onCreate Process by BlunoLibrary
+		getPermission();//授权
+
+		onCreateProcess();														//onCreate Process by BlunoLibrary
+
 
 
         serialBegin(115200);													//set the Uart Baudrate on BLE chip to 115200
@@ -111,6 +118,49 @@ public class MainActivity  extends BlunoLibrary {
 		serialReceivedText.append(theString);							//append the text into the EditText
 		//The Serial data from the BLUNO may be sub-packaged, so using a buffer to hold the String is a good choice.
 		((ScrollView)serialReceivedText.getParent()).fullScroll(View.FOCUS_DOWN);
+	}
+
+	@SuppressLint("WrongConstant")
+	private void getPermission() {
+//		if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+		if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+			System.out.println("ok!!!!!");
+			int permissionCheck = 0;
+			permissionCheck = this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+			permissionCheck += this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION);
+			if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+				System.out.println("get!!!!!");
+				this.requestPermissions( // 请求授权
+						new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+								Manifest.permission.ACCESS_COARSE_LOCATION},
+						ACCESS_LOCATION);// 自定义常量,任意整型
+			} else {
+				// 已经获得权限
+				System.out.println("already!!!!!");
+			}
+		}
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		switch (requestCode) {
+			case ACCESS_LOCATION:
+				if (hasAllPermissionGranted(grantResults)) {
+//                    Log.d(TAG, "onRequestPermissionsResult: OK");
+				} else {
+//                    Log.d(TAG, "onRequestPermissionsResult: NOT OK");
+				}
+				break;
+		}
+	}
+
+	private boolean hasAllPermissionGranted(int[] grantResults) {
+		for (int grantResult : grantResults) {
+			if (grantResult == PackageManager.PERMISSION_DENIED) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
